11c520843daf985187857936028f6bb0c64d99bd
Суховаров Антон <raptor6564@gmail.com>
2026-08-30 09:50:19 +0300

    vim-tmux-navigator

--- home/nvim.nix
+++ home/nvim.nix
@@ -212,6 +212,21 @@
             require("lualine").setup()
           end,
         },
+
+        {
+          "christoomey/vim-tmux-navigator",
+          cmd = {
+            "TmuxNavigateLeft", "TmuxNavigateDown", "TmuxNavigateUp",
+            "TmuxNavigateRight", "TmuxNavigatePrevious",
+          },
+          keys = {
+            { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+          },
+        },
       })
 
       -- Базовые настройки редактора
